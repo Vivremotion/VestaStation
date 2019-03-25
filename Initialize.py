9043a3f1c15c48674208f20548fdcde258df5361
tandy02 <cyril.tandy@etu.unilim.fr>
2019-03-25 22:05:51 +0100

    Add Initialization module

---
+++ Initialize.py
@@ -0,0 +1,50 @@
+from BluetoothModule import BluetoothModule;
+import Station;
+from Firebase import firebase;
+import importlib;
+import time;
+import threading;
+
+sensors = ['TemperatureHumidity']
+
+class Initializer():
+    def bluetooth(self):
+        bluetoothModule = BluetoothModule()
+        bluetoothModule.initialize(1)
+        print('Bluetooth initialized')
+
+    def sendMeasurements(self):
+        while True:
+            for moduleName in sensors:
+                module = importlib.import_module(moduleName)
+                values = module.get(None, None)
+                if not isinstance(values, list):
+                    values = [values]
+                for value in values:
+                    value.update({ 'id': self.station['id'] })
+                    firebase.send('measurements', None, value)
+                print(moduleName+' measurement sent to firestore')
+            time.sleep(self.station['settings']['measurementsInterval'])
+
+    def initialize(self):
+        Station.initialize()
+        self.station = Station.get(None, None)
+        print('Station:')
+        print(self.station)
+        firebase.initialize()
+        print('Firebase initialized')
+        if not 'id' in self.station:
+            document = firebase.send('stations', None, self.station)
+            Station.set(None, {
+                'id': document.id
+            })
+            self.station['id'] = document.id
+            print('New station added to online database')
+
+        bluetoothThread = threading.Thread(target=self.bluetooth)
+        bluetoothThread.start()
+        measurementsThread = threading.Thread(target=self.sendMeasurements)
+        measurementsThread.start()
+
+initializer = Initializer()
+initializer.initialize()
