2868b15eeacc0a8ffdb39f29023b674133307adc
tandy02 <cyril.tandy@etu.unilim.fr>
2019-03-29 21:11:43 +0100

    Add addWriter and addReader methods on the Station module

--- Station.py
+++ Station.py
@@ -16,7 +16,13 @@ def initialize():
         file = open(stationFilePath, 'a+')
         file.write(json.dumps(station))
 
-def get(parameters=None, data=None):
+def _edit(station):
+    file = open(stationFilePath, 'w')
+    file.write(json.dumps(station))
+    firebase.send('stations', station['id'], station)
+    return station
+
+def get(data=None):
     initialize()
     file = open(stationFilePath, 'r')
     fileContent = file.read()
@@ -24,18 +30,32 @@ def get(parameters=None, data=None):
     station = json.loads(fileContent)
     return station
 
-def set(parameters, data):
+def set(data):
     initialize()
-    station = get(None, None)
+    station = get()
     if 'settings' in data:
         station['settings'].update(data['settings'])
         data['settings'] = station['settings']
     station.update(data)
-    print(station)
-    file = open(stationFilePath, 'w')
-    file.write(json.dumps(station))
     if 'name' in station['settings']:
         file = open('/etc/machine-info', 'w')
         file.write('PRETTY_HOSTNAME='+station['settings']['name'])
-    firebase.send('stations', station['id'], station)
+    _edit(station)
     return station
+
+def addWriter(data):
+    if not data['uid']:
+        return { 'error': 'Missing uid' }
+    initialize()
+    station = get()
+    if not data['uid'] in station['write']
+    # if the user has not the read right on the station, add them
+    return 'OK'
+    
+def addReader(data):
+    if not data['uid']:
+        return { 'error': 'Missing uid' }
+    initialize()
+    station = get()
+    # if the user has not the read right on the station, add them
+    return 'OK'
